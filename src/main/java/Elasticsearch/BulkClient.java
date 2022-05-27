4d38909af9250d2fc5c80e4d59121e8435c16771
stosicms <milos.stosic.sud@gmail.com>
2022-05-27 14:07:10 +0200

    2

--- src/main/java/Elasticsearch/BulkClient.java
+++ src/main/java/Elasticsearch/BulkClient.java
@@ -1,20 +1,26 @@
 package Elasticsearch;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
 import co.elastic.clients.elasticsearch.indices.ExistsRequest;
-import co.elastic.clients.transport.endpoints.BooleanResponse;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.xcontent.XContentBuilder;
 
 import java.io.IOException;
 
 public class BulkClient {
     ElasticsearchClient client = ElasticClient.getClientInstance();
 
-    private RestHighLevelClient checkIfIndexExists () throws IOException {
-         result = client.indices().exists(ExistsRequest.of(e->e.index("books")));
-        return result;
+    private boolean checkIfIndexExists (String index) throws IOException {
+        var result = client.indices().exists(ExistsRequest.of(e->e.index(index)));
+        return result.value();
     }
-    private void makeIndex() throws IOException {
-        if (checkIfIndexExists()) {
+    void makeIndex (String index) throws IOException {
+        if (!checkIfIndexExists(index)) {
+            CreateIndexRequest indexRequest = new CreateIndexRequest(index);
+
+            CreateIndexResponse createIndexResponse = this.client.indices().create(indexRequest, RequestOptions.DEFAULT);
+        }
     }
 }
